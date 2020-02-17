07e815c3ccea44e8d9aba08ad12c674918022b3c
BEUMGOO <captain_kbg@naver.com>
2020-02-17 23:54:55 +0900

    stack navigator 02

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -2,11 +2,17 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import TabNavigation from "./TabNavigation";
 import DetailScreen from "../screens/Detail";
+import { headerStyles } from "./config";
 
 const MainNavigation = createStackNavigator(
   {
-    Tabs: { screen: TabNavigation, navigationOptions: { header: null } },
-    Detail: DetailScreen
+    Tabs: { screen: TabNavigation, navigationOptions: { headerShown: false } },
+    Detail: {
+      screen: DetailScreen,
+      navigationOptions: {
+        ...headerStyles
+      }
+    }
   },
   {}
 );
