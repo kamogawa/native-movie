a3cb7cba5832032736253f9c76d7f04b97aef338
BEUMGOO <captain_kbg@naver.com>
2020-02-27 23:49:59 +0900

    tv container

--- screens/Movies/MoviesContainer.js
+++ screens/Movies/MoviesContainer.js
@@ -12,20 +12,40 @@ export default class extends React.Component {
   };
 
   async componentDidMount() {
+    let upcoming, popular, nowPlaying, error;
     try {
-      const upcoming = await movies.getUpcoming();
-      const popular = await movies.getPopular();
-      const nowPlaying = await movies.getNowPlaying();
-      console.log(upcoming, popular, nowPlaying);
-    } catch {
-      this.setState({ error: "Can't get Movies." });
+      ({
+        data: { results: upcoming }
+      } = await movies.getUpcoming());
+      ({
+        data: { results: popular }
+      } = await movies.getPopular());
+      ({
+        data: { results: nowPlaying }
+      } = await movies.getNowPlaying());
+    } catch (error) {
+      console.log(error);
+      error = "Can't get Movies.";
     } finally {
-      this.setState({ loading: false });
+      this.setState({
+        loading: false,
+        error,
+        upcoming,
+        popular,
+        nowPlaying
+      });
     }
   }
 
   render() {
-    const { loading } = this.state;
-    return <MoviesPresenter loading={loading} />;
+    const { loading, upcoming, popular, nowPlaying } = this.state;
+    return (
+      <MoviesPresenter
+        upcoming={upcoming}
+        popular={popular}
+        loading={loading}
+        nowPlaying={nowPlaying}
+      />
+    );
   }
 }
